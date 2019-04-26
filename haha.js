92e9211cf48f47d54e9032122e38abd4ebe32ab2
lklindar <48013189+lklindar@users.noreply.github.com>
2019-04-26 14:27:35 +0700

    COBA

---
+++ haha.js
@@ -0,0 +1,8 @@
+﻿function kirim() {
+     var a=document.getElementById("abc").innerHTML;
+     var b=document.getElementById("teks").value;
+     var c=document.getElementById("kepala").value;
+     document.write(a + "<br />" + "<br />");
+     document.write(b + "<br />");
+     document.write(c);
+  }
